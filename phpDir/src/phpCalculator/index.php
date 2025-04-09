d8044cb12fbbdd95e000ae3e389742735c21b8f0
Huttunenbe <beniittahuttunen@gmail.com>
2025-04-09 10:07:42 +0300

    change calculator to separate folder

--- phpDir/src/phpCalculator/index.php
+++ phpDir/src/phpCalculator/index.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Simple PHP Calculator</title>
-    <link rel="stylesheet" href="simpleCalculator.css">
+    <link rel="stylesheet" href="styles.css">
 </head>
 
 <body>
